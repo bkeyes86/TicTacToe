6ed77ccaca8cb9f7deddf1b508b054c7f6748297
bkeyes86 <byronvkeyes@gmail.com>
2020-08-30 16:53:01 -0500

    some things going

---
+++ UnitTesting/Board.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
+{
+	public class Board
+	{
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public  string[,] GameBoard = new string[,]
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+	
+
+	public void DisplayBoard()
+		{
+			//TODO: Output the board to the console
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write($"|{GameBoard[i, j]}|");
+
+				}
+
+				Console.Write("\n");
+
+			}
+
+			Console.ReadLine();
+
+			
+
+		}
+	}
+}
